7a1ee680f1ece6df4c52f726a53ed96e1ee04cd8
Skyice <vespade@gmail.com>
2017-10-17 00:11:39 +0200

    Bug con

--- commands/sangroyale/show-event.js
+++ commands/sangroyale/show-event.js
@@ -29,29 +29,35 @@ class ShowEventCommand extends Commando.Command{
 				var synthesis = "Tous les évenements prévus dans Sang Royale :" + "\n"
 				body=JSON.parse(body)
 				var event=body[0]
+				if(event != undefined){
 
-				var kind=""
-				if(event.tournamentChallongeProperty != null){
-					kind="Tournoi Challonge"
-				} else {
-					kind="Tournoi à gemmes"
+					var kind=""
+					if(event.tournamentChallongeProperty != null){
+						kind="Tournoi Challonge"
+					} else {
+						kind="Tournoi à gemmes"
+					}
+
+					synthesis += "```"
+					synthesis += "Nom : " + event.name + " / Id : " + event.id  + "\n"
+					synthesis += "Date : "  + event.date + "\n"
+					synthesis += "Type : " + kind + "\n"
+					if(kind === "Tournoi à gemmes"){
+						synthesis += "Nombre gemmes : " + event.tournamentGemProperty.gemnumber + "\n"
+						synthesis += "Mot de passe : " + event.tournamentGemProperty.password + "\n"
+					}
+					synthesis += "Capacité : " + event.capacity + "\n"
+					synthesis += "Organisateur : " + event.organizer + "\n"
+					synthesis += "Visibilité : " + event.privacy + "\n"
+					if(event.description != "") synthesis += "Description : " + event.description + "\n"
+						synthesis += "```"
+
+					message.channel.sendMessage(synthesis)
+
+
+
 				}
-
-				synthesis += "```"
-				synthesis += "Nom : " + event.name + " / Id : " + event.id  + "\n"
-				synthesis += "Date : "  + event.date + "\n"
-				synthesis += "Type : " + kind + "\n"
-				if(kind === "Tournoi à gemmes"){
-					synthesis += "Nombre gemmes : " + event.tournamentGemProperty.gemnumber + "\n"
-					synthesis += "Mot de passe : " + event.tournamentGemProperty.password + "\n"
-				}
-				synthesis += "Capacité : " + event.capacity + "\n"
-				synthesis += "Organisateur : " + event.organizer + "\n"
-				synthesis += "Visibilité : " + event.privacy + "\n"
-				if(event.description != "") synthesis += "Description : " + event.description + "\n"
-				synthesis += "```"
-
-				message.channel.sendMessage(synthesis)
+				
 
 			}).catch(function(err){
 				message.channel.sendMessage("Erreur inconnue : encore la faute à Skyice")
